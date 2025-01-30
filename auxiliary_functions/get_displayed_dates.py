dd9a7c61aedb34e89f9279324077f721846e850a
pablo-fdz <pablo.fdzalb@gmail.com>
2025-01-30 09:45:40 +0100

    removed all files

--- auxiliary_functions/get_displayed_dates.py
+++
@@ -1,4 +0,0 @@
-# Extract all dates displayed in the calendar
-def get_displayed_dates(browser, by_type, path):
-    dates = browser.find_elements(by = by_type, value = path)
-    return [date.get_attribute("data-date") for date in dates]
